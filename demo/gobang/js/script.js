12774eddc08efdcb1d585470f412130bdb4a37e1
wikeliyi <1245788423@qq.com>
2016-03-31 01:16:39 +0800

    update

--- demo/gobang/js/script.js
+++ demo/gobang/js/script.js
@@ -5,7 +5,29 @@
 var chess = document.getElementById("chess");
 var context = chess.getContext("2d");
 
+
+var chessBoard=[];
+var me=true;
+
+for(var i=0;i<15;i++){
+    chessBoard[i]=[];
+    for(var j=0;i<15;j++){
+        chessBoard[i][j]=0;
+    }
+}
+
 context.strokeStyle="#bfbfbf";
+
+var logo=new Image();
+logo.src="images/logo.png";
+logo.onload=function(){
+    context.drawImage(logo,0,0,450,450);
+    drawChessBoard();
+    oneStep(0,0,true);
+    oneStep(1,1,false);
+}
+
+
 
 for(var i=0;i<25;i++){
     context.moveTo(15+i*30,15);
@@ -26,5 +48,22 @@ gradient.addColorStop(1,"#636766");
 
 context.fillStyle=gradient;
 context.fill();
+
+var oneStep=function(i,hj,me) {
+    context.beginPath();
+    context.arc(15 + i * 30, 15 + j * 30, 13, 0, 2 * Math * PI);
+    context.closePath();
+
+    var gradient = context.createRadialGradient(15 + i * 30 + 2, 15 + j * 30 + 2);
+    if (me) {
+        gradient.addColorStop(0£¬"#0a0a0a"
+    )
+        ;
+        gradient.addColorStop(1, "#636766");
+    } else {
+        gradient.addColorStop(0, "#d1d1d1");
+        gradient.addColorStop(1, "#F9f9f9");
+    }
+}
 
 //alert("sdf");
