2b7f706e5f7e116fe0477a12c97bccffd1b56b91
wikeliyi <1245788423@qq.com>
2016-04-26 20:44:43 +0800

    update

--- demo/weather/js/weather.js
+++ demo/weather/js/weather.js
@@ -1,8 +1,9 @@
-$(document).on("pagebeforeshow", "#page1", function(){
-	var urlCity = "https://api.heweather.com/x3/weather?cityid=CN101200101&key=ead00e8e8c2540c9a87af54bb35002cf";
-	var _strCity = {};
-	showDetail();
+var urlCity = "https://api.heweather.com/x3/weather?cityid=CN101200101&key=ead00e8e8c2540c9a87af54bb35002cf";
+var _strCity = {};
 
+
+$(document).on("pagebeforeshow", "#page1", function(){
+	getAjax();
 	//dom事件
 	$(".select").on("click", function(event){
 		alert("hello");
@@ -18,8 +19,10 @@ $(document).on("pagebeforeshow", "#page1", function(){
 	});
 	$(".refresh").on("tap", function(){
 		window.location.reload();
-		showDetail();
+		//showDetail();
+		getAjax();
 	});
+});
 
 	//locationQuery();
 
@@ -62,69 +65,16 @@ $(document).on("pagebeforeshow", "#page1", function(){
 	//});
 
 	//ajax
-	function showDetail(){
-		if (window.XMLHttpRequest) {
-			var oAjax=new XMLHttpRequest;
-		}
-		else{
-			var oAjax=new ActiveXObject('Microsoft.XMLHTTP');
-		}
-		oAjax.open('GET', urlCity, true);
-		oAjax.send(null);
-		oAjax.onreadystatechange=function(){
-			if (oAjax.readyState==4) {
-				if (oAjax.status==200) {
-					_strCity = JSON.parse(oAjax.responseText);
-					$(".refresh p").html(_strCity["HeWeather data service 3.0"][0]["basic"]["update"]["loc"].substring(11)+'更新');
-					$(".page1_pos span").html(_strCity["HeWeather data service 3.0"][0]["basic"]["city"]);
-					$(".aqi .aqi_l").html(_strCity["HeWeather data service 3.0"][0]["aqi"]["city"]["aqi"]);
-					$(".aqi .aqi_r").html('&nbsp;&nbsp;&nbsp;&nbsp;'+_strCity["HeWeather data service 3.0"][0]["aqi"]["city"]["qlty"]);
-					$(".aqi .pm25_r").html('&nbsp;&nbsp;&nbsp;&nbsp;'+_strCity["HeWeather data service 3.0"][0]["aqi"]["city"]["pm25"]);
-					$(".basic .tmp").html(_strCity["HeWeather data service 3.0"][0]["now"]["tmp"]+'℃');
-					$(".basic .basic_txt").html(_strCity["HeWeather data service 3.0"][0]["now"]["cond"]["txt"]);
-					$(".basic .basic_max").html(_strCity["HeWeather data service 3.0"][0]["daily_forecast"][0]["tmp"]["max"] + '℃');
-					$(".basic .basic_min").html('&nbsp;&nbsp;&nbsp;&nbsp;'+_strCity["HeWeather data service 3.0"][0]["daily_forecast"][0]["tmp"]["min"] + '℃');
-					$(".basic .basic_hum").html('&nbsp;&nbsp;&nbsp;&nbsp;'+_strCity["HeWeather data service 3.0"][0]["now"]["hum"] + '%');
-					$(".basic .basic_dir").html(_strCity["HeWeather data service 3.0"][0]["now"]["wind"]["dir"]);
-					$(".basic .basic_sc").html('&nbsp;&nbsp;&nbsp;&nbsp;'+_strCity["HeWeather data service 3.0"][0]["now"]["wind"]["sc"]+'级');
-					$(".basic .basic_sr").html('日出'+_strCity["HeWeather data service 3.0"][0]["daily_forecast"][0]["astro"]["sr"]);
-					$(".basic .basic_ss").html('&nbsp;&nbsp;&nbsp;&nbsp;'+'日落'+_strCity["HeWeather data service 3.0"][0]["daily_forecast"][0]["astro"]["ss"]);
-					$(".comf span").html(_strCity["HeWeather data service 3.0"][0]["suggestion"]["comf"]["brf"]);
-					$(".drsg span").html(_strCity["HeWeather data service 3.0"][0]["suggestion"]["drsg"]["brf"]);
-					$(".uv span").html(_strCity["HeWeather data service 3.0"][0]["suggestion"]["uv"]["brf"]);
-					$(".cw span").html(_strCity["HeWeather data service 3.0"][0]["suggestion"]["cw"]["brf"]);
-					$(".trav span").html(_strCity["HeWeather data service 3.0"][0]["suggestion"]["trav"]["brf"]);
-					$(".flu span").html(_strCity["HeWeather data service 3.0"][0]["suggestion"]["flu"]["brf"]);
-					$(".sport span").html(_strCity["HeWeather data service 3.0"][0]["suggestion"]["sport"]["brf"]);
-					$("#comf p").html(_strCity["HeWeather data service 3.0"][0]["suggestion"]["comf"]["txt"]);
-					$("#drsg p").html(_strCity["HeWeather data service 3.0"][0]["suggestion"]["drsg"]["txt"]);
-					$("#uv p").html(_strCity["HeWeather data service 3.0"][0]["suggestion"]["uv"]["txt"]);
-					$("#cw p").html(_strCity["HeWeather data service 3.0"][0]["suggestion"]["cw"]["txt"]);
-					$("#trav p").html(_strCity["HeWeather data service 3.0"][0]["suggestion"]["trav"]["txt"]);
-					$("#flu p").html(_strCity["HeWeather data service 3.0"][0]["suggestion"]["flu"]["txt"]);
-					$("#sport p").html(_strCity["HeWeather data service 3.0"][0]["suggestion"]["sport"]["txt"]);
 
-					var hourly_forecast = _strCity["HeWeather data service 3.0"][0]["hourly_forecast"];
-					var trContent1 = "";
-					var trContent2 = "";
-					var trContent3 = "";
-					var trContent4 = "";
-					for (var i = 0; i < hourly_forecast.length; i++) {
-						trContent1 += '<td>' + hourly_forecast[i]["date"].substring(11) + '</td>';
-						trContent2 += '<td>' + hourly_forecast[i]["tmp"] + '℃</td>';
-						trContent3 += '<td>' + hourly_forecast[i]["wind"]["dir"] + '</td>';
-						trContent4 += '<td>' + hourly_forecast[i]["wind"]["sc"] + '</td>';
-					}
-					document.getElementsByTagName("table")[0].innerHTML = '<tr>' + trContent1 + '</tr>';
-					document.getElementsByTagName("table")[0].innerHTML += '<tr>' + trContent2 + '</tr>';
-					document.getElementsByTagName("table")[0].innerHTML += '<tr>' + trContent3 + '</tr>';
-					document.getElementsByTagName("table")[0].innerHTML += '<tr>' + trContent4 + '</tr>';
-				}
-			}
-		};
-	}
 
-});
+
+
+
+	//ajax
+
+
+
+
 /*页面二*/
 $(document).on("pagebeforeshow", "#page2", function(){
 	var urlCity = "https://api.heweather.com/x3/weather?cityid=CN101200101&key=ead00e8e8c2540c9a87af54bb35002cf";
@@ -195,3 +145,74 @@ $(document).on("pagebeforeshow", "#page2", function(){
 		}
 	}
 });
+
+
+function getAjax(_urlCity){
+	if (window.XMLHttpRequest) {
+		var oAjax=new XMLHttpRequest;
+	}
+	else{
+		var oAjax=new ActiveXObject('Microsoft.XMLHTTP');
+	}
+
+	if(_urlCity!=undefined){
+		oAjax.open('GET', _urlCity, true);
+		oAjax.send(null);
+		oAjax.onreadystatechange=function() {
+			if (oAjax.readyState == 4) {
+				if (oAjax.status == 200) {
+					_strCity = JSON.parse(oAjax.responseText);
+					showDetail();
+				}
+			}
+		}
+	}
+}
+
+
+function showDetail() {
+	$(".refresh p").html(_strCity["HeWeather data service 3.0"][0]["basic"]["update"]["loc"].substring(11) + '更新');
+	$(".page1_pos span").html(_strCity["HeWeather data service 3.0"][0]["basic"]["city"]);
+	$(".aqi .aqi_l").html(_strCity["HeWeather data service 3.0"][0]["aqi"]["city"]["aqi"]);
+	$(".aqi .aqi_r").html('&nbsp;&nbsp;&nbsp;&nbsp;' + _strCity["HeWeather data service 3.0"][0]["aqi"]["city"]["qlty"]);
+	$(".aqi .pm25_r").html('&nbsp;&nbsp;&nbsp;&nbsp;' + _strCity["HeWeather data service 3.0"][0]["aqi"]["city"]["pm25"]);
+	$(".basic .tmp").html(_strCity["HeWeather data service 3.0"][0]["now"]["tmp"] + '℃');
+	$(".basic .basic_txt").html(_strCity["HeWeather data service 3.0"][0]["now"]["cond"]["txt"]);
+	$(".basic .basic_max").html(_strCity["HeWeather data service 3.0"][0]["daily_forecast"][0]["tmp"]["max"] + '℃');
+	$(".basic .basic_min").html('&nbsp;&nbsp;&nbsp;&nbsp;' + _strCity["HeWeather data service 3.0"][0]["daily_forecast"][0]["tmp"]["min"] + '℃');
+	$(".basic .basic_hum").html('&nbsp;&nbsp;&nbsp;&nbsp;' + _strCity["HeWeather data service 3.0"][0]["now"]["hum"] + '%');
+	$(".basic .basic_dir").html(_strCity["HeWeather data service 3.0"][0]["now"]["wind"]["dir"]);
+	$(".basic .basic_sc").html('&nbsp;&nbsp;&nbsp;&nbsp;' + _strCity["HeWeather data service 3.0"][0]["now"]["wind"]["sc"] + '级');
+	$(".basic .basic_sr").html('日出' + _strCity["HeWeather data service 3.0"][0]["daily_forecast"][0]["astro"]["sr"]);
+	$(".basic .basic_ss").html('&nbsp;&nbsp;&nbsp;&nbsp;' + '日落' + _strCity["HeWeather data service 3.0"][0]["daily_forecast"][0]["astro"]["ss"]);
+	$(".comf span").html(_strCity["HeWeather data service 3.0"][0]["suggestion"]["comf"]["brf"]);
+	$(".drsg span").html(_strCity["HeWeather data service 3.0"][0]["suggestion"]["drsg"]["brf"]);
+	$(".uv span").html(_strCity["HeWeather data service 3.0"][0]["suggestion"]["uv"]["brf"]);
+	$(".cw span").html(_strCity["HeWeather data service 3.0"][0]["suggestion"]["cw"]["brf"]);
+	$(".trav span").html(_strCity["HeWeather data service 3.0"][0]["suggestion"]["trav"]["brf"]);
+	$(".flu span").html(_strCity["HeWeather data service 3.0"][0]["suggestion"]["flu"]["brf"]);
+	$(".sport span").html(_strCity["HeWeather data service 3.0"][0]["suggestion"]["sport"]["brf"]);
+	$("#comf p").html(_strCity["HeWeather data service 3.0"][0]["suggestion"]["comf"]["txt"]);
+	$("#drsg p").html(_strCity["HeWeather data service 3.0"][0]["suggestion"]["drsg"]["txt"]);
+	$("#uv p").html(_strCity["HeWeather data service 3.0"][0]["suggestion"]["uv"]["txt"]);
+	$("#cw p").html(_strCity["HeWeather data service 3.0"][0]["suggestion"]["cw"]["txt"]);
+	$("#trav p").html(_strCity["HeWeather data service 3.0"][0]["suggestion"]["trav"]["txt"]);
+	$("#flu p").html(_strCity["HeWeather data service 3.0"][0]["suggestion"]["flu"]["txt"]);
+	$("#sport p").html(_strCity["HeWeather data service 3.0"][0]["suggestion"]["sport"]["txt"]);
+
+	var hourly_forecast = _strCity["HeWeather data service 3.0"][0]["hourly_forecast"];
+	var trContent1 = "";
+	var trContent2 = "";
+	var trContent3 = "";
+	var trContent4 = "";
+	for (var i = 0; i < hourly_forecast.length; i++) {
+		trContent1 += '<td>' + hourly_forecast[i]["date"].substring(11) + '</td>';
+		trContent2 += '<td>' + hourly_forecast[i]["tmp"] + '℃</td>';
+		trContent3 += '<td>' + hourly_forecast[i]["wind"]["dir"] + '</td>';
+		trContent4 += '<td>' + hourly_forecast[i]["wind"]["sc"] + '</td>';
+	}
+	document.getElementsByTagName("table")[0].innerHTML = '<tr>' + trContent1 + '</tr>';
+	document.getElementsByTagName("table")[0].innerHTML += '<tr>' + trContent2 + '</tr>';
+	document.getElementsByTagName("table")[0].innerHTML += '<tr>' + trContent3 + '</tr>';
+	document.getElementsByTagName("table")[0].innerHTML += '<tr>' + trContent4 + '</tr>';
+}
