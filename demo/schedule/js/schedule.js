4b34b5d0aa2ce9628087f12293d1ef015ea59c4b
wikeliyi <1245788423@qq.com>
2016-05-05 12:45:37 +0800

    Merge remote-tracking branch 'origin/gh-pages'
    # Conflicts:
    #	demo/form/index.html

--- demo/schedule/js/schedule.js
+++ demo/schedule/js/schedule.js
@@ -6,6 +6,25 @@
  */
 
 
+//页面的事件绑定
+$(document).on("pagebeforeshow", "#page1", function() {
+    var now = new Date();
+
+    //section1
+    showDateTime(now);
+
+    //section2
+    //获取下一节课的课堂次数
+    var courseTimer = getNowTimer(now);
+
+    showSubjectSection(now,$("#firstCourse"),courseTimer,0);
+    //section3
+    showSubjectSection(now,$("#secondCourse"),courseTimer+1,1);
+
+
+});
+
+
 $(document).on("pagebeforeshow", "#page2", function() {
 
     var time = $(".time-weeks");
@@ -18,17 +37,17 @@ $(document).on("pagebeforeshow", "#page2", function() {
     for(var i=0 ;i<5;i++){
         var course_am = $(".course:eq("+i+") .time-course-am");
         $.each(course_am,function(j){
-            $(this).text(weeks[i].am.subject[j]);
+            $(this).text(weeks[i].am.subject[j].cname);
         });
 
         var course_pm = $(".course:eq("+i+") .time-course-pm");
         $.each(course_pm,function(j){
-            $(this).text(weeks[i].pm.subject[j]);
+            $(this).text(weeks[i].pm.subject[j].cname);
         });
 
         var course_ng = $(".course:eq("+i+") .time-course-ng");
         $.each(course_ng,function(j){
-            $(this).text(weeks[i].ng.subject[j]);
+            $(this).text(weeks[i].ng.subject[j].cname);
         });
     }
 
@@ -67,3 +86,98 @@ $(".select").on("click", function(){
 $(document).on("click", function(){
     $(".selectDown").css("display", "none");
 });
+
+
+function getDataTime(){
+    var now = new Date();
+    console.log(now.getHours()+":"+now.getMinutes());
+}
+
+getDataTime();
+
+
+//显示当周的日期和天气情况
+function showDateTime(now){
+    var week = ["星期日","星期一","星期二","星期三","星期四","星期五","星期六"];
+    var hour = now.getHours();
+    var minute = now.getMinutes();
+
+    if(hour<10){
+        hour="0"+hour;
+    }
+    if(minute<10){
+        minute="0"+minute;
+    }
+
+    var current = $("#dateMessage");
+    current.find(".current-time").html(hour+":"+minute);
+    current.find(".current-date").html((now.getMonth()+1)+"月"+now.getDate()+"日");
+    current.find(".current-week").html(week[now.getDay()]);
+}
+
+
+//
+function getNowTimer(now){
+    var timer = [830,920,1025,1115,1400,1450,1535,1630,1715,1830,1920,2005,2055];
+    var time = now.getHours()*100+now.getMinutes();
+    var nestTime = 0;
+
+
+    //不仅要判断当前的时间，还要处理改时间段是否有课
+    for(var i=0;i<timer.length;i++){
+        if(timer[i]>time){
+            nestTime = i;
+            break;
+        }
+    }
+    return nestTime;
+}
+
+
+function showSubjectSection(now,id,courseTimer,type){
+
+    var timer = [830,920,1025,1115,1400,1450,1535,1630,1715,1830,1920,2005,2055];
+    var weeker = now.getDay();
+
+    //var courseTimer = getNowTimer(now)+1;
+    //var firstCourse = $("#secondCourse");
+    var subj = id.find(".course-subject");
+    var lastTime = id.find(".last-time");
+    var teacN = id.find(".teacher-name");
+    var location = id.find(".class-location");
+    var time = id.find(".class-time");
+
+
+    var nowTime = now.getHours()*100+now.getMinutes();
+    if(timer[courseTimer]-nowTime > 0){
+
+        var b60 = Math.floor(timer[courseTimer]/100)*60+timer[courseTimer]%100;
+        var a60 = +(Math.floor(nowTime/100)*60+nowTime%100);
+
+        lastTime.html(Math.floor((b60-a60)/60)+":"+((b60-a60)%60));
+
+        if(courseTimer<4){
+            subj.html(weeks[weeker].am.subject[courseTimer].cname);
+            teacN.html(weeks[weeker].am.subject[courseTimer].teacher);
+            location.html(weeks[weeker].am.subject[courseTimer].location)
+
+        }else if(courseTimer<8){
+            subj.html(weeks[weeker].pm.subject[courseTimer-4].cname);
+            teacN.html(weeks[weeker].am.subject[courseTimer-4].teacher);
+            location.html(weeks[weeker].am.subject[courseTimer-4].location)
+        }else if(courseTimer<12){
+            subj.html(weeks[weeker].pm.subject[courseTimer-8].cname);
+            teacN.html(weeks[weeker].am.subject[courseTimer-8].teacher);
+            location.html(weeks[weeker].am.subject[courseTimer-8].location)
+        }
+        time.html(Math.floor(timer[courseTimer]/100)+":"+Math.floor(timer[courseTimer]%100)%60);
+    }
+    else{
+        subj.html(weeks[(weeker+1)%5].am.subject[type].cname);
+        teacN.html(weeks[(weeker+1)%5].am.subject[type].teacher);
+        location.html(weeks[(weeker+1)%5].am.subject[type].location);
+        time.html(Math.floor(timer[type]/100)+":"+timer[type]%100);
+        lastTime.html("明天")
+    }
+}
+
--- demo/schedule/js/week.js
+++ demo/schedule/js/week.js
@@ -35,87 +35,410 @@ var subject =[
 
 
 
-//var course = {
-//    英语:English,
-//    数学:Maths,
-//    语文:Chinese
-//};
-
-
 var weeks=[
     {
         name:"星期一",
         am:{
             name:"上午",
-            subject:[subject[0].cname,subject[1].cname,subject[2].cname,subject[0].cname]
+            subject: [{
+                name :"chinese",
+                cname:"语文",
+                teacher:"tom",
+                location:"8-415"
+            },
+            {
+                name:"english",
+                cname:"英语",
+                teacher:"hetao",
+                location:"8-415"
+
+            },{
+                name : "maths",
+                cname:"数学",
+                teacher:"fan",
+                location:"8-415"
+            },{
+                name :"chinese",
+                cname:"语文",
+                teacher:"tom",
+                location:"8-415"
+            }]
         },
         pm:{
             name:"下午",
-            subject:[subject[0].cname,subject[1].cname,subject[2].cname,subject[0].cname]
+            subject:[{
+                name :"chinese",
+                cname:"语文",
+                teacher:"tom",
+                location:"8-415"
+            },
+            {
+                name:"english",
+                cname:"英语",
+                teacher:"hetao",
+                location:"8-415"
+
+            },{
+                name : "maths",
+                cname:"数学",
+                teacher:"fan",
+                location:"8-415"
+            },{
+                name :"chinese",
+                cname:"语文",
+                teacher:"tom",
+                location:"8-415"
+            }]
         },
         ng:{
             name:"晚上",
-            subject:[subject[0].cname,subject[1].cname,subject[2].cname,subject[0].cname]
+            subject:[{
+                name :"chinese",
+                cname:"语文",
+                teacher:"tom",
+                location:"8-415"
+            },
+            {
+                name:"english",
+                cname:"英语",
+                teacher:"hetao",
+                location:"8-415"
+
+            },{
+                name : "maths",
+                cname:"数学",
+                teacher:"fan",
+                location:"8-415"
+            },{
+                name :"chinese",
+                cname:"语文",
+                teacher:"tom",
+                location:"8-415"
+            }]
         }
     },
     {
         name:"星期二",
         am:{
             name:"上午",
-            subject:[subject[0].cname,subject[1].cname,subject[2].cname,subject[0].cname]
+            subject:[{
+                name :"chinese",
+                cname:"语文",
+                teacher:"tom",
+                location:"8-415"
+            },
+            {
+                name:"english",
+                cname:"英语",
+                teacher:"hetao",
+                location:"8-415"
+
+            },{
+                name : "maths",
+                cname:"数学",
+                teacher:"fan",
+                location:"8-415"
+            },{
+                name :"chinese",
+                cname:"语文",
+                teacher:"tom",
+                location:"8-415"
+            }]
         },
         pm:{
             name:"下午",
-            subject:[subject[0].cname,subject[1].cname,subject[2].cname,subject[0].cname]
+            subject:[{
+                name :"chinese",
+                cname:"语文",
+                teacher:"tom",
+                location:"8-415"
+            },
+            {
+                name:"english",
+                cname:"英语",
+                teacher:"hetao",
+                location:"8-415"
+
+            },{
+                name : "maths",
+                cname:"数学",
+                teacher:"fan",
+                location:"8-415"
+            },{
+                name :"chinese",
+                cname:"语文",
+                teacher:"tom",
+                location:"8-415"
+            }]
         },
         ng:{
             name:"晚上",
-            subject:[subject[0].cname,subject[1].cname,subject[2].cname,subject[0].cname]
-        }
+            subject:[{
+                name :"chinese",
+                cname:"语文",
+                teacher:"tom",
+                location:"8-415"
+            },
+            {
+                name:"english",
+                cname:"英语",
+                teacher:"hetao",
+                location:"8-415"
+
+            },{
+                name : "maths",
+                cname:"数学",
+                teacher:"fan",
+                location:"8-415"
+            },{
+                name :"chinese",
+                cname:"语文",
+                teacher:"tom",
+                location:"8-415"
+            }]
+    }
     },
     {
         name:"星期三",
         am:{
             name:"上午",
-            subject:[subject[0].cname,subject[1].cname,subject[2].cname,subject[0].cname]
+            subject:[{
+                name :"chinese",
+                cname:"语文",
+                teacher:"tom",
+                location:"8-415"
+            },
+            {
+                name:"english",
+                cname:"英语",
+                teacher:"hetao",
+                location:"8-415"
+
+            },{
+                name : "maths",
+                cname:"数学",
+                teacher:"fan",
+                location:"8-415"
+            },{
+                name :"chinese",
+                cname:"语文",
+                teacher:"tom",
+                location:"8-415"
+            }]
         },
         pm:{
             name:"下午",
-            subject:[subject[0].cname,subject[1].cname,subject[2].cname,subject[0].cname]
+            subject:[{
+                name :"chinese",
+                cname:"语文",
+                teacher:"tom",
+                location:"8-415"
+            },
+            {
+                name:"english",
+                cname:"英语",
+                teacher:"hetao",
+                location:"8-415"
+
+            },{
+                name : "maths",
+                cname:"数学",
+                teacher:"fan",
+                location:"8-415"
+            },{
+                name :"chinese",
+                cname:"语文",
+                teacher:"tom",
+                location:"8-415"
+            }]
         },
         ng:{
             name:"晚上",
-            subject:[subject[0].cname,subject[1].cname,subject[2].cname,subject[0].cname]
+            subject:[{
+                name :"chinese",
+                cname:"语文",
+                teacher:"tom",
+                location:"8-415"
+            },
+            {
+                name:"english",
+                cname:"英语",
+                teacher:"hetao",
+                location:"8-415"
+
+            },{
+                name : "maths",
+                cname:"数学",
+                teacher:"fan",
+                location:"8-415"
+            },{
+                name :"chinese",
+                cname:"语文",
+                teacher:"tom",
+                location:"8-415"
+            }]
         }
     },
     {
         name:"星期四",
         am:{
             name:"上午",
-            subject:[subject[0].cname,subject[1].cname,subject[2].cname,subject[0].cname]
+            subject:[{
+                name :"chinese",
+                cname:"语文",
+                teacher:"tom",
+                location:"8-415"
+            },
+            {
+                name:"english",
+                cname:"英语",
+                teacher:"hetao",
+                location:"8-415"
+
+            },{
+                name : "maths",
+                cname:"数学",
+                teacher:"fan",
+                location:"8-415"
+            },{
+                name :"chinese",
+                cname:"语文",
+                teacher:"tom",
+                location:"8-415"
+            }]
         },
         pm:{
             name:"下午",
-            subject:[subject[0].cname,subject[1].cname,subject[2].cname,subject[0].cname]
+            subject:[{
+                name :"chinese",
+                cname:"语文",
+                teacher:"tom",
+                location:"8-415"
+            },
+            {
+                name:"english",
+                cname:"英语",
+                teacher:"hetao",
+                location:"8-415"
+
+            },{
+                name : "maths",
+                cname:"数学",
+                teacher:"fan",
+                location:"8-415"
+            },{
+                name :"chinese",
+                cname:"语文",
+                teacher:"tom",
+                location:"8-415"
+            }]
         },
         ng:{
             name:"晚上",
-            subject:[subject[0].cname,subject[1].cname,subject[2].cname,subject[0].cname]
+            subject:[{
+                name :"chinese",
+                cname:"语文",
+                teacher:"tom",
+                location:"8-415"
+            },
+            {
+                name:"english",
+                cname:"英语",
+                teacher:"hetao",
+                location:"8-415"
+
+            },{
+                name : "maths",
+                cname:"数学",
+                teacher:"fan",
+                location:"8-415"
+            },{
+                name :"chinese",
+                cname:"语文",
+                teacher:"tom",
+                location:"8-415"
+            }]
         }
     },
     {
         name:"星期五",
         am:{
             name:"上午",
-            subject:[subject[0].cname,subject[1].cname,subject[2].cname,subject[0].cname]
+            subject:[{
+                name :"chinese",
+                cname:"语文",
+                teacher:"tom",
+                location:"8-415"
+            },
+            {
+                name:"english",
+                cname:"英语",
+                teacher:"hetao",
+                location:"8-415"
+
+            },{
+                name : "maths",
+                cname:"数学",
+                teacher:"fan",
+                location:"8-415"
+            },{
+                name :"chinese",
+                cname:"语文",
+                teacher:"tom",
+                location:"8-415"
+            }]
         },
         pm:{
             name:"下午",
-            subject:[subject[0].cname,subject[1].cname,subject[2].cname,subject[0].cname]
+            subject:[{
+                name :"chinese",
+                cname:"语文",
+                teacher:"tom",
+                location:"8-415"
+            },
+            {
+                name:"english",
+                cname:"英语",
+                teacher:"hetao",
+                location:"8-415"
+
+            },{
+                name : "maths",
+                cname:"数学",
+                teacher:"fan",
+                location:"8-415"
+            },{
+                name :"chinese",
+                cname:"语文",
+                teacher:"tom",
+                location:"8-415"
+            }]
         },
         ng:{
             name:"晚上",
-            subject:[subject[0].cname,subject[1].cname,subject[2].cname,subject[0].cname]
+            subject:[{
+                name :"chinese",
+                cname:"语文",
+                teacher:"tom",
+                location:"8-415"
+            },
+            {
+                name:"english",
+                cname:"英语",
+                teacher:"hetao",
+                location:"8-415"
+
+            },{
+                name : "maths",
+                cname:"数学",
+                teacher:"fan",
+                location:"8-415"
+            },{
+                name :"chinese",
+                cname:"语文",
+                teacher:"tom",
+                location:"8-415"
+            }]
         }
     },
 ];
